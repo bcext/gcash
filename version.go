73081f9e1aeac7a387ebc63dac616c4db2bab115
qshuai <qishuai231@gmail.com>
2018-11-18 14:34:42 +0800

    version: Adjust the client version to v2.0.0

--- version.go
+++ version.go
@@ -17,7 +17,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // These constants define the application version and follow the semantic
 // versioning 2.0.0 spec (http://semver.org/).
 const (
-	appMajor uint = 1
+	appMajor uint = 2
 	appMinor uint = 0
 	appPatch uint = 0
 	// appPreRelease MUST only contain characters from semanticAlphabet
